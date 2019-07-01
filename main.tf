3ca8d17ae4c2620303623fa49788f9cea9a25432
Alex Harness <9669770+mocofound@users.noreply.github.com>
2019-07-01 10:26:46 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ resource "azurerm_app_service_plan" "test" {
   name                = "api-appserviceplan-iso"
   location            = "${azurerm_resource_group.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
-  kind                = "Windows"
-  #reserved            = true
+  kind                = "Linux"
+  reserved            = true
 
   sku {
     tier = "Isolated"
