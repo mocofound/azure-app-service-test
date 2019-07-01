31a96e6633ed2945e8837596221340f2bcdb3b8f
Alex Harness <9669770+mocofound@users.noreply.github.com>
2019-07-01 09:33:13 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "azurerm" {
+    version = "~> 1.31.0"
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "api-rg-isolated"
+  location = "US Central"
+}
+
+resource "azurerm_app_service_plan" "test" {
+  name                = "api-appserviceplan-pro"
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Isolated"
+    size = "I1"
+  }
+}
