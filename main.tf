42fef847a321c973608ff9abc30ff4893c625244
Alex Harness <9669770+mocofound@users.noreply.github.com>
2019-07-01 09:57:01 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,11 +4,11 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "test" {
   name     = "api-rg-isolated"
-  location = "Central US"
+  location = "Eastern US"
 }
 
 resource "azurerm_app_service_plan" "test" {
-  name                = "api-appserviceplan-pro"
+  name                = "api-appserviceplan-iso"
   location            = "${azurerm_resource_group.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
   kind                = "Linux"
@@ -17,5 +17,6 @@ resource "azurerm_app_service_plan" "test" {
   sku {
     tier = "Isolated"
     size = "I1"
+    capacity = 1
   }
 }
