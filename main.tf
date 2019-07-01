476ea6aba0527d8c12d96ce79312c5341f8f26fd
Alex Harness <9669770+mocofound@users.noreply.github.com>
2019-07-01 10:34:52 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "azurerm_app_service_plan" "test" {
   location            = "${azurerm_resource_group.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
   kind                = "Linux"
-  reserved            = true
+  #reserved            = true
 
   sku {
     tier = "Isolated"
